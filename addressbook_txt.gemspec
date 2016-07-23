411326087c0a5c8c2c6f26e00047d414a3709932
Your Name <you@example.com>
2016-07-23 19:46:37 +0100

    1st commit

---
+++ addressbook_txt.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'addressbook_txt'
+  s.version = '0.1.0'
+  s.summary = 'Stores address book entries in a plain text file'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/addressbook_txt.rb']
+  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.12')
+  s.signing_key = '../privatekeys/addressbook_txt.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/addressbook_txt'
+end
---
+++ lib/addressbook_txt.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/env ruby
+
+# file: addressbook_txt.rb
+
+require 'date'
+require 'dynarex'
+require 'fileutils'
+
+
+
+# The AddressbookTxt gem can:
+#
+#  [x] create a new addressbook.txt file
+#  [x] read an existing addressbook.txt file
+#  [ ] search each entry using a keyword
+#  [ ] archive address entries on an annual basis
+#  [ ] search the archive using a keyword
+
+
+
+class AddressbookTxt
+  
+  attr_reader :to_s
+
+  def initialize(filename='addressbook.txt', path: '.')
+    
+    @filename, @path = filename, path
+    
+    fpath = File.join(path, filename)
+    
+    if File.exists?(fpath) then
+
+      @dx = import_to_dx(File.read(fpath))    
+          
+    else
+      @dx = new_dx    
+    end
+  end
+
+  def dx()
+    @dx
+  end
+  
+  def save(filename=@filename)
+
+    s = File.basename(filename) + "\n" + dx_to_s(@dx).lines[1..-1].join
+    File.write File.join(@path, filename), s
+    @dx.save File.join(@path, filename.sub(/\.txt$/,'.xml'))
+        
+  end
+  
+  def to_s()
+    dx_to_s @dx
+  end
+
+  private
+  
+  def dx_to_s(dx)
+   
+    title = File.basename(@filename)
+    title + "\n" + "=" * title.length + "\n\n%s\n\n" % [dx.to_s(header: false)]
+
+  end  
+  
+  def import_to_dx(s)
+
+    a = s.lines
+    # Remove the heading
+    a.shift 2   
+    
+    # add the sectionx raw document type
+    a.unshift '--#'
+
+    dx = new_dx()
+    dx.import(a.join)
+    
+    return dx
+  end
+  
+  def new_dx()
+    
+    dx = Dynarex.new 'addresses[title]/address(x)'
+    dx.title = "Address Book"
+
+    return dx
+
+  end
+  
+
+end
